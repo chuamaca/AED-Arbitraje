c386d20cd5abbd3b3c504c692cb9821f34919f06
chuamaca <cesarhuamanicastro@gmail.com>
2023-09-07 23:45:02 -0500

    initial proyect

---
+++ src/main/java/com/dev/aed/arbitraje/AEDArbitraje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dev.aed.arbitraje;
+
+/**
+ *
+ * @author Cesar
+ */
+public class AEDArbitraje {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
