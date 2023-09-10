c2605d0339d02bd1f1a3dc98998710db4f86ee0e
JhoanGP17 <144413428+JhoanGP17@users.noreply.github.com>
2023-09-10 16:42:09 -0500

    Diseño
    Creacion diseño

--- src/main/java/com/dev/aed/arbitraje/Utils/ConexionJDBC.java
+++ src/main/java/com/dev/aed/arbitraje/Utils/ConexionJDBC.java
@@ -24,7 +24,7 @@ public class ConexionJDBC {
         String conexionServer = "jdbc:sqlserver://DCODE\\SQLEXPRESS01;"
                 + "database=AEDArbitraje;"
                 +"user=sa;"
-                +"password=123;"
+                +"password=1234;"
                 +"loginTimeout=30;"
                  +"encrypt=true;"+
                 "trustServerCertificate=True;";
