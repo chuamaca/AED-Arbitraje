e14c1433b865210f1c11ae1a9376facbdc279707
JhoanGP17 <144413428+JhoanGP17@users.noreply.github.com>
2023-11-20 00:15:08 -0500

    Actualizando

---
+++ src/main/java/com/dev/aed/arbitraje/cmbAsigArbi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dev.aed.arbitraje;
+
+import javax.swing.DefaultComboBoxModel;
+
+/**
+ *
+ * @author jhoan
+ */
+class cmbAsigArbi {
+
+    static void setModel(DefaultComboBoxModel<String> comboBoxModelEstado) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
