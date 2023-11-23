de038f347de0664ba6178a37ba0c9f63700a266c
chuamaca <cesarhuamanicastro@gmail.com>
2023-11-22 19:48:49 -0500

    Add gestio

---
+++ src/main/java/com/dev/aed/arbitraje/Model/MGestionarDemanda.java
@@ -0,0 +1,47 @@
+package com.dev.aed.arbitraje.Model;
+
+import java.sql.Date;
+
+public class MGestionarDemanda {
+
+
+    public String EstadoAudiencia;
+    public String ResultadoAudiencia;
+    public String FechaAudiencia;
+
+    public MGestionarDemanda() {
+    }
+
+    public MGestionarDemanda(String EstadoAudiencia, String ResultadoAudiencia, String FechaAudiencia) {
+        this.EstadoAudiencia = EstadoAudiencia;
+        this.ResultadoAudiencia = ResultadoAudiencia;
+        this.FechaAudiencia = FechaAudiencia;
+    }
+
+    public String getEstadoAudiencia() {
+        return EstadoAudiencia;
+    }
+
+    public void setEstadoAudiencia(String EstadoAudiencia) {
+        this.EstadoAudiencia = EstadoAudiencia;
+    }
+
+    public String getResultadoAudiencia() {
+        return ResultadoAudiencia;
+    }
+
+    public void setResultadoAudiencia(String ResultadoAudiencia) {
+        this.ResultadoAudiencia = ResultadoAudiencia;
+    }
+
+    public String getFechaAudiencia() {
+        return FechaAudiencia;
+    }
+
+    public void setFechaAudiencia(String FechaAudiencia) {
+        this.FechaAudiencia = FechaAudiencia;
+    }
+    
+    
+    
+}
